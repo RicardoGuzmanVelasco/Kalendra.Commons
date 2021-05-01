1f583c7c12981f033dff86ebffe6eedf391d7679
RGV <ricardo.guzman.velasco@outlook.com>
2021-05-02 00:56:39 +0200

    Common infrastructure basics

---
+++ Runtime/Infraestructure/Services/UnityEngineRandomService.cs
@@ -0,0 +1,59 @@
+﻿using Kalendra.Commons.Runtime.Architecture.Services;
+using UnityEngine;
+
+namespace Kalendra.Commons.Runtime.Infraestructure.Services
+{
+    /// <summary>
+    /// Performance: 1000 requests : 0.1s, 100000 requests: 98s.
+    /// </summary>
+    public sealed class UnityEngineRandomService : AbstractRandomService
+    {
+        int timesRequested;
+        int currentSeed;
+        
+        #region Accesors
+        public override int Seed
+        {
+            set
+            {
+                currentSeed = value;
+                Random.InitState(value);
+                
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public UnityEngineRandomService(int seed) => Seed = seed;
+        #endregion
+
+        #region Random providing
+        public override int Next(int inclusiveMin, int exclusiveMax)
+        {
+            ResetSeed();
+            return Random.Range(inclusiveMin, exclusiveMax);
+        }
+        
+        public override float Next()
+        {
+            ResetSeed();
+            return Random.value;
+        }
+
+        public override float Next(float inclusiveMin, float inclusiveMax)
+        {
+            ResetSeed();
+            return Random.Range(inclusiveMin, inclusiveMax);
+        }
+        #endregion
+        
+        void ResetSeed()
+        {
+            Random.InitState(currentSeed);
+            for(var i = 0; i < timesRequested; i++)
+                Random.Range(0, 1f);
+            timesRequested++;
+        }
+
+    }
+}
---
+++ Runtime/Infraestructure/Services/UnityMathService.cs
@@ -0,0 +1,45 @@
+﻿using Kalendra.Commons.Runtime.Architecture.Services;
+using UnityEngine;
+
+namespace Kalendra.Commons.Runtime.Infraestructure.Services
+{
+    public sealed class UnityMathService : IMathService
+    {
+        #region IMathClampService implementation
+        public int Clamp(int value, int min, int max)
+        {
+            return Mathf.Clamp(value, min, max);
+        }
+
+        public int InverseClamp(int value, int min, int max)
+        {
+            if(value < min)
+                value = max;
+            if(value > max)
+                value = min;
+            return value;
+        }
+
+        public int MirrorClamp(int value, int min, int max)
+        {
+            while(value < min)
+                value += max - min + 1;
+            while(value > max)
+                value -= max - min + 1;
+            return value;
+        }
+        #endregion
+        
+        #region IMathTupleService implementation
+        public (int x, int y) Add((int x, int y) t1, (int x, int y) t2)
+        {
+            return (t1.x + t2.x, t1.y + t2.y);
+        }
+
+        public bool AreNegativeVectors((int x, int y) t1, (int x, int y) t2)
+        {
+            return Add(t1, t2) == (0, 0);
+        }
+        #endregion
+    }
+}
